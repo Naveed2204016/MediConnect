e53c4064117ad3aff8f22093777b2218d6113518
naveed <khan10naveed@gmail.com>
2025-08-04 06:46:55 -0700

    icon added

--- src/main/java/mediconnect/main.java
+++ src/main/java/mediconnect/main.java
@@ -3,6 +3,7 @@ package mediconnect;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class main extends Application {
@@ -11,7 +12,9 @@ public class main extends Application {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/welcome.fxml"));
         Scene scene = new Scene(loader.load());
         primaryStage.setTitle("MediConnect");
+        Image image =new Image(getClass().getResource("/images/1583625.png").toExternalForm());
         primaryStage.setScene(scene);
+        primaryStage.getIcons().add(image);
         primaryStage.show();
     }
 
