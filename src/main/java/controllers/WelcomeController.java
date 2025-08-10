dbbf842ca79ca10828f1be10ad874ca6be885fcb
naveed <khan10naveed@gmail.com>
2025-08-10 12:37:53 -0700

    changed some buttons' colour

--- src/main/java/controllers/WelcomeController.java
+++ src/main/java/controllers/WelcomeController.java
@@ -1,9 +1,12 @@
 package controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,6 +20,23 @@ public class WelcomeController {
     @FXML private Button loginButton;
     @FXML private ToggleGroup roleToggleGroup;
     @FXML private Button signupButton;
+
+    // Reference to the VBox in <center> where we will insert the image
+    @FXML private VBox centerVBox;
+
+   /* @FXML
+    public void initialize() {
+        // Load image
+        Image bannerImage = new Image(getClass().getResourceAsStream("/images/PersianArt.jpg")); // change path as needed
+        ImageView bannerImageView = new ImageView(bannerImage);
+
+        // Make image stretch horizontally but keep aspect ratio
+        bannerImageView.setPreserveRatio(true);
+        bannerImageView.setFitWidth(750); // almost the width of your center area
+
+        // Insert image at the top of VBox
+        centerVBox.getChildren().add(0, bannerImageView);
+    }*/
 
     @FXML
     private void onLoginButtonClick() throws IOException {
@@ -44,15 +64,13 @@ public class WelcomeController {
         }
     }
 
-    private void loadloginsScene(String role,String fxmlPath) throws IOException
-    {
+    private void loadloginsScene(String role,String fxmlPath) throws IOException {
         FXMLLoader loader=new FXMLLoader(getClass().getResource(fxmlPath));
         Scene scene=new Scene(loader.load());
         LoginController loginController=loader.getController();
         loginController.setRole(role);
         Stage stage=(Stage) loginButton.getScene().getWindow();
         stage.setScene(scene);
-
     }
 
     private void loadScene(String fxmlPath) throws IOException {
